b512f78cbd06021613c522402e3a4a606d37f51d
Sarah Richens <sarahgrichens@gmail.com>
2023-03-12 09:49:08 -0500

    took off foreign key on belongs to many - don't need

--- models/index.js
+++ models/index.js
@@ -16,14 +16,12 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: ProductTag,
-  foreignKey: 'product_id'
+  through: ProductTag,  
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: ProductTag,
-  foreignKey: 'tag_id'
+  through: ProductTag,  
 })
 
 module.exports = {
